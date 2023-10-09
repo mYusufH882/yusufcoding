3095ef315ad3e5be7abbfc040c1b42c18b3b112f
YusufCoding <haryadim28@gmail.com>
2023-10-09 23:45:18 +0700

    Update web with clean code data

---
+++ js/portfolio.js
@@ -0,0 +1,64 @@
+var dataPortfolio = {
+    "projek": [
+
+        {
+            "judul": "Web Event CSRG (Computer Security Research Group)",
+            "deskripsi": "Web untuk menampilkan informasi acara CSRG di kampus STMIK AMIKBANDUNG.",
+            "thumbnail": "Web Event CSRG.png",
+            "gambar": [
+                "Web Event CSRG.png",
+                "Admin-CSRG.png"
+            ]
+        },
+        {
+            "judul": "Web CaiKopi (Capaian Kinerja Koperasi)",
+            "deskripsi": "Web untuk penilaian kinerja koperasi yang ada di kota Cimahi.",
+            "thumbnail": "Cai-Kopi.png",
+            "gambar": [
+                "Cai-Kopi.png",
+                "Admin-Dashboard-CaiKopi.png"
+            ]
+        },
+        {
+            "judul": "Chimasistaker v2 FrontEnd (Disnaker)",
+            "deskripsi": "Web disnaker untuk portal informasi pekerjaan kepada pencari kerja di kota cimahi",
+            "thumbnail": "Disnaker.png",
+            "gambar": [
+                "Disnaker.png"
+            ]
+        },
+        {
+            "judul": "Chimasistaker v2 Dashboard",
+            "deskripsi": "Dashboard admin untuk mengelola data pencari kerja kota cimahi.",
+            "thumbnail": "Chimasistaker Dashboard.png",
+            "gambar": [
+                "Chimasistaker Dashboard.png"
+            ]
+        },
+        {
+            "judul": "Inovasi Dashboard",
+            "deskripsi": "Dashboard admin untuk mengelola data inovasi di kota cimahi",
+            "thumbnail": "Web Inovasi Dashboard.png",
+            "gambar": [
+                "Web Inovasi Dashboard.png"
+            ]
+        },
+        {
+            "judul": "Landing Page Web Chima Award",
+            "deskripsi": "Landing page untuk menampilkan data inovasi dari Inovasi Dashboard",
+            "thumbnail": "Chima Awards 2022.png",
+            "gambar": [
+                "Chima Awards 2022.png"
+            ]
+        },
+        {
+            "judul": "Web Portal Media Pemuda Persis",
+            "deskripsi": "Web portal untuk menampilkan/mempublikasikan informasi artikel, acara atau kegiatan pemuda persis cimahi selatan.",
+            "thumbnail": "Web PemudaPersisCimsel.png",
+            "gambar": [
+                "Web PemudaPersisCimsel.png",
+                "Admin PemudaPersisCimsel.png"
+            ]
+        }
+    ]
+}
